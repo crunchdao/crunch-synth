fed6f9e0f079eeb2a2a9b0eef12584493b970b7d
Tarandro <alexis.gassmann2@orange.fr>
2026-03-27 22:55:02 +0100

    release 0.9.0

--- crunch_synth/__version__.py
+++ crunch_synth/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'crunch-synth'
 __description__ = 'A package for participating in the Crunch-Synth Game on CrunchDAO'
-__version__ = '0.8.0'
+__version__ = '0.9.0'
 __author__ = [
     "Abdennour BOUTRIG",
     "Alexis Gassmann",
